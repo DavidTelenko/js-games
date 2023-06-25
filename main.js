96c2275e326e40d57b55fafb32f8309de568d613
David Telen'ko <davidtelenko11@gmail.com>
2023-06-25 07:45:35 +0300

    stylish

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
-class Int32Matrix {
-    constructor(rows, cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.data = new Int32Array(rows * cols);
-    }
-
-    get(rowIndex, colIndex) {
-        const index = this.index(rowIndex, colIndex);
-        return this.data[index];
-    }
-
-    set(rowIndex, colIndex, value) {
-        const index = this.index(rowIndex, colIndex);
-        this.data[index] = value;
-    }
-
-    index(rowIndex, colIndex) {
-        // Make sure the row and column indices are valid
-        if (rowIndex < 0 || rowIndex >= this.rows ||
-            colIndex < 0 || colIndex >= this.cols) {
-            throw new Error(`Invalid indices: (${rowIndex}, ${colIndex})`);
-        }
-
-        // Calculate the one-dimensional index from the row and column indices
-        return rowIndex * this.cols + colIndex;
-    }
+function getRandomInRange(min, max) {
+    return Math.random() * (max - min) + min
 }
 
 class Board {
     constructor(size) {
-        this.size = size;
-        this.data = new Int32Matrix(size, size);
+        this.size = size
+        this.len = this.size * this.size
+        this.data = new Int32Array(this.len)
+        this.init()
+    }
+
+    generateNew() {
+        return Math.random() > 0.25 ? 1 : 2
+    }
+
+    init() {
+        this.data.fill(0)
+
+        for (let i = 0; i < Math.round(getRandomInRange(2, 4)); i++) {
+            const index = Math.round(getRandomInRange(0, this.len - 1))
+            this.data[index] = this.generateNew()
+        }
     }
 
     moveLeft() {
@@ -49,8 +39,25 @@ class Board {
 
     }
 
-    render(view) {
-        view.render(this.data)
+    get(rowIndex, colIndex) {
+        const index = this.index(rowIndex, colIndex)
+        return super[index]
+    }
+
+    set(rowIndex, colIndex, value) {
+        const index = this.index(rowIndex, colIndex)
+        super[index] = value
+    }
+
+    index(rowIndex, colIndex) {
+        // Make sure the row and column indices are valid
+        if (rowIndex < 0 || rowIndex >= this.size ||
+            colIndex < 0 || colIndex >= this.size) {
+            throw new Error(`Invalid indices: (${rowIndex}, ${colIndex})`)
+        }
+
+        // Calculate the one-dimensional index from the row and column indices
+        return rowIndex * this.size + colIndex
     }
 
     update(view) {
@@ -62,33 +69,42 @@ class HTMLBoardView {
     constructor(htmlBoard, size) {
         this.board = htmlBoard
         this.size = size
+        this.len = this.size * this.size
         this.init()
     }
 
     init() {
-        this.board.style.gridTemplateColumns = `repeat(${this.size}, 1fr)`;
+        this.board.style.gridTemplateColumns = `repeat(${this.size}, minmax(0, 1fr))`
 
-        for (let i = 0; i < this.size * this.size; i++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            this.board.appendChild(cell);
+        for (let i = 0; i < this.len; i++) {
+            const cell = document.createElement("div")
+            cell.classList.add("cell")
+            cell.id = i.toString()
+            this.board.appendChild(cell)
         }
     }
 
-    render(data) {
-
-    }
-
     update(data) {
-
+        for (let i = 0; i < this.len; i++) {
+            const id = i.toString()
+            let cell = document.getElementById(id)
+            if (data[i] == 0) {
+                continue
+            }
+            const value = Math.pow(2, data[i])
+            cell.textContent = value.toString()
+            cell.classList.add(`num${value}`)
+        }
     }
 }
 
 function initBoard() {
-    const board = document.querySelector(".board");
+    const board = document.querySelector(".board")
     const size = 4
     let boardView = new HTMLBoardView(board, size)
     let boardModel = new Board(size)
+
+    boardModel.update(boardView)
 }
 
 initBoard()
